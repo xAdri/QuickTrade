76b976a74de024a78aa7adb1f7db27ab3280d69e
xAdri <aperezgfx@gmail.com>
2020-12-13 23:14:36 +0100

    AE7: Pantalla insertar producto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,44 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  tecnologia: string = "tecnologia";
+  hogar: string = "hogar";
+  inmobiliaria: string = "inmobiliaria";
+  motor: string = "motor";
+
+  mostrarOpcion: string = "";
+
+  mostrarTecnologia: boolean = false;
+  mostrarMotor: boolean = false;
+  mostrarInmobiliaria: boolean = false;
+  constructor() { }
+
+  mostrarPanel(): void {
+
+    if (this.mostrarOpcion == this.tecnologia) {
+      this.mostrarTecnologia = true;
+      this.mostrarMotor = false;
+      this.mostrarInmobiliaria = false;
+    }
+
+    if (this.mostrarOpcion == this.hogar) {
+      this.mostrarTecnologia = false;
+      this.mostrarMotor = false;
+      this.mostrarInmobiliaria = false;
+    }
+
+    if (this.mostrarOpcion == this.inmobiliaria) {
+      this.mostrarInmobiliaria = true;
+      this.mostrarTecnologia = false;
+      this.mostrarMotor = false;
+    }
+
+    if (this.mostrarOpcion == this.motor) {
+      this.mostrarMotor = true;
+      this.mostrarTecnologia = false;
+      this.mostrarInmobiliaria = false;
+    }
+
+  }
 
 }
